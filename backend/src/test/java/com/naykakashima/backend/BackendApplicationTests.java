5cfa1b5d7a98d0a49e5cf284348c2ccb69f4f447
naykakashima <porgimat@gmail.com>
2025-11-11 19:30:15 +0000

    Add backend structure with Maven configuration and initial application files

---
+++ backend/src/test/java/com/naykakashima/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naykakashima.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
