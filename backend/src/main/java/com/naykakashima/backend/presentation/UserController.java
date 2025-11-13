9a803bf1818afbf735b7e5a974e0058b73fc80e2
naykakashima <porgimat@gmail.com>
2025-11-13 14:18:34 +0000

    Add UserController with createUser and getUserByFirebaseUid endpoints

---
+++ backend/src/main/java/com/naykakashima/backend/presentation/UserController.java
@@ -0,0 +1,27 @@
+package com.naykakashima.backend.presentation;
+
+import com.naykakashima.backend.application.UserService;
+import com.naykakashima.backend.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService  userService;
+    
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = userService.createUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @GetMapping("/{firebaseUid}")
+    public ResponseEntity<User> getUserByFirebaseUid(@PathVariable String firebaseUid){
+        return userService.getUserByFirebaseUid(firebaseUid)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
