2d1298a752c572689bff65fb15f41d769d3c6a17
naykakashima <porgimat@gmail.com>
2025-11-17 21:32:20 +0000

    Add Firebase configuration and dotenv support to the project

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -4,6 +4,12 @@ module.exports = function (api) {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
+      ["module:react-native-dotenv", {
+        "moduleName": "@env",
+        "path": ".env",
+        "safe": false,
+        "allowUndefined": true
+      }]
     ],
   };
 };
